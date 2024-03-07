b2c0f25ff47b7263b56f1686fb71f0d4220a7c0b
ArthurLDH <arthurldh@gmail.com>
2024-03-07 15:11:12 +0100

    add: uuid support

--- src/Entity/Seller.php
+++ src/Entity/Seller.php
@@ -1,19 +1,21 @@
 <?php
-//
-//namespace App\Entity;
-//
-//use Doctrine\ORM\Mapping as ORM;
-//
-//#[ORM\Entity]
-//#[ORM\Table(name: 'seller')]
-//class Seller
-//{
-//    #[ORM\Id]
-//    #[ORM\Column(type: 'uuid')]
-//    #[ORM\GeneratedValue(strategy: "CUSTOM")]
-//    #[ORM\CustomIdGenerator(class: "Ramsey\Uuid\Generator")]
-//    private ?string $uuid = null;
-//
-//    #[ORM\Column(length: 255, unique: true)]
-//    private ?string $email = null;
-//}
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+use Ramsey\Uuid\UuidInterface;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'seller')]
+class Seller
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private ?UuidInterface $uid = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $email = null;
+}
